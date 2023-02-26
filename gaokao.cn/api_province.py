80ec94dc99fc462f0f6df05f657c4fb82fe0022d
Guijin Ding <dingguijin@gmail.com>
2023-02-26 09:24:09 +0800

    province type

--- gaokao.cn/api_province.py
+++ gaokao.cn/api_province.py
@@ -8,7 +8,7 @@ def get_provinces_ids():
     ps = x["data"]["province"]
     return ps
 
-def get_provinces_types():
+def get_provinces_types(year="2022"):
     f = open("province_control.json")
     x = f.read()
     f.close()
@@ -17,7 +17,7 @@ def get_provinces_types():
     ps = x["data"]["province"]
     types = x["data"]["type"]
 
-    keys = list(map(lambda p: "%s_2022" % p, ps))
+    keys = list(map(lambda p: "%s_%s" % (p, year), ps))
     ktypes = list(map(lambda k: types[k], keys))
     
     return dict(zip(ps, ktypes))
